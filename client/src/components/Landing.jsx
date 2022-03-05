74d20cd44fcea24bc7b2b057c42d90e2475a1d5d
Alejo <alejoivanrojas@gmail.com>
2022-03-05 18:26:32 -0300

    landing css added

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import landingVideo from "../assets/landingVideo.mp4";
+import "../styles/Landing.modulate.css";
 
 const Landing = () => {
   return (
-    <>
-    <h1>Welcomed: DogDeck</h1>
-    <Link to='/home'>
-        <button>Home</button>
-    </Link>
+    <div className="landing-container">
+      <video
+        className="video"
+        src={landingVideo}
+        controls=""
+        muted
+        autoPlay={"autoplay"}
+        preload="auto"
+        loop
+      />
+      <h1>Find your perfect dog</h1>
+      <h3>Start checking and pick your favourites</h3>
+      <Link to="/home">
+        <button className="home-btn" >HOME</button>
+      </Link>
+      
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default Landing
+export default Landing;
