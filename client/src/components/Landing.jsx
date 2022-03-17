21cd622c9fb267d127b65377038dbe0042016d2f
Alejo <alejoivanrojas@gmail.com>
2022-03-17 09:06:44 -0300

    slider fixed

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,27 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import landingVideo from "../assets/landingVideo.mp4";
 import styles from "../styles/Landing.module.css";
-import backLanding from "../assets/backLanding.jpg";
+import Slider from "./Slider";
 
 const Landing = () => {
   return (
     <div className={styles.landingContainer}>
-      {/* <video
-        className={styles.video}
-        src={landingVideo}
-        controls=""
-        muted
-        autoPlay={"autoplay"}
-        preload="auto"
-        loop
-      /> */}
-      <img src={backLanding} alt="dog slide 1" />
-      <h1>Find your perfect dog</h1>
-      <h3>Start checking and pick your favourites</h3>
-      <Link to="/home">
-        <button className={styles.homeBtn}>HOME</button>
-      </Link>
+      <Slider />
     </div>
   );
 };
--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import styles from "../styles/Slider.module.css";
-import slider8 from "../assets/slider8.jpg";
-import slider2 from "../assets/slider2.jpg";
 import slider3 from "../assets/slider3.jpg";
 import slider6 from "../assets/slider6.jpg";
+import slider10 from "../assets/slider10.jpg";
+import backLanding from "../assets/backLanding.jpg";
+import { Link } from "react-router-dom";
 
 function Slider() {
   return (
     <div className={styles.slider}>
       <figure>
-        <img src={slider6} alt='dog slide 1'/>
-        <img src={slider2}alt='dog slide 2' />
-        <img src={slider3} alt='dog slide 3'/>
-        <img src={slider8} alt='dog slide 4'/>
-        <img src={slider6} alt='dog slide 5'/>
+        <img src={backLanding} alt="dog slide 1" />
+        <img src={slider3} alt="dog slide 2" />
+        <img src={slider6} alt="dog slide 3" />
+        <img src={slider10} alt="dog slide 4" />
+        <img src={backLanding} alt="dog slide 5" />
       </figure>
+      <div className={styles.landingContainer}>
+        <h1>Find your perfect dog</h1>
+        <h3>Start checking and pick your favourites</h3>
+        <Link to="/home">
+          <button className={styles.homeBtn}>HOME</button>
+        </Link>
+      </div>
     </div>
   );
 }
