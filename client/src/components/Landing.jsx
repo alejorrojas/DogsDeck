b81d938f66c5b80c8be7e57275fefd009f88210a
Alejo <alejoivanrojas@gmail.com>
2022-03-17 00:31:54 -0300

    landing changed

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import landingVideo from "../assets/landingVideo.mp4";
 import styles from "../styles/Landing.module.css";
+import backLanding from "../assets/backLanding.jpg";
 
 const Landing = () => {
   return (
     <div className={styles.landingContainer}>
-      <video
+      {/* <video
         className={styles.video}
         src={landingVideo}
         controls=""
@@ -14,11 +15,12 @@ const Landing = () => {
         autoPlay={"autoplay"}
         preload="auto"
         loop
-      />
+      /> */}
+      <img src={backLanding} alt="dog slide 1" />
       <h1>Find your perfect dog</h1>
       <h3>Start checking and pick your favourites</h3>
       <Link to="/home">
-        <button className={styles.homeBtn }>HOME</button>
+        <button className={styles.homeBtn}>HOME</button>
       </Link>
     </div>
   );
